ae7b5ca2328d8ca751c99063db092af54e1452af
admin <vadim771@ukr.net>
2023-07-24 00:19:17 +0300

    hello its remaked homework

--- script.js
+++ script.js
@@ -1,84 +1,93 @@
-function addNumbers(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) {
-      alert("Error: Please enter valid numbers");
-      return NaN;
-    }
-    return num1 + num2;
+//////////////////// home work for 24 lesson //////////////////////
+
+
+
+
+function calculation() {
+  let num1 = parseFloat(prompt("Enter first numberito please"))
+
+  let num2 = parseFloat(prompt("Enter second numberito please"))
+
+  if (isNaN(num1) || isNaN(num2)) {
+      alert('you have entered not a number for calculation')
+      console.log('Error you need to enter numbers to calculate')
+      } else {
+  function multiplier(num1, num2) {
+      return num1 * num2 
   }
-  
-  function subtractNumbers(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) {
-      alert("Error: Please enter valid numbers");
-      return NaN;
-    }
-    return num1 - num2;
-  }
-  
-  function multiplyNumbers(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) {
-      alert("Error: Please enter valid numbers");
-      return NaN;
-    }
-    return num1 * num2;
-  }
-  
-  function divideNumbers(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) {
-      alert("Error: Please enter valid numbers");
-      return NaN;
-    } else if (num2 === 0) {
-      alert("Error: Cannot divide by zero");
-      return NaN;
-    }
-    return num1 / num2;
-  }
-  
-  function findMinimumNumber(arr) {
-    if (!Array.isArray(arr)) {
-      alert("Error: Please enter an array");
-      return NaN;
-    }
-    let minNum = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] < minNum) {
-        minNum = arr[i];
+
+  function difference(num1, num2) {
+      if (num2 > num1) {
+          alert(window.confirm('Your first number is smaller then second, do you really want to continue?'))
+          return num1 - num2;
+      } else {
+      return num1 - num2;
       }
-    }
-    return minNum;
   }
-  
-  function findMaximumNumber(arr) {
-    if (!Array.isArray(arr)) {
-      alert("Error: Please enter an array");
-      return NaN;
-    }
-    let maxNum = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] > maxNum) {
-        maxNum = arr[i];
+
+  function sum(num1, num2) {
+      return num1 + num2;
+  }
+
+  function division(num1, num2) {
+      if (num2 === 0) {
+          alert('Devision by zero is not allowed at this time')
+          return 'Division failed'
+      } else {
+          return num1 / num2;
       }
-    }
-    return maxNum;
   }
+  let multiplierResult = multiplier(num1, num2);
+  let differenceResult = difference(num1, num2);
+  let sumResult = sum(num1, num2);
+  let divisionResult = division(num1, num2);
+  console.log(`The myltiply result is: ${multiplierResult}.\n\rThe difference result is: ${differenceResult}.\n\rThe sum of numbers is: ${sumResult}.\n\rThe division of numbers are: ${divisionResult}`)
+  return
+  }
+}
+
+calculation();
+
+//////////////// Function with arrey max and min numbers /////////////////////////
+
+
+function findMaxNumber(){
+  let b = [];
+
+  for (i = 0; i < 51; i++)
+
+  b.push(i);
   
-  let num1 = prompt("Enter the first number:");
-  let num2 = prompt("Enter the second number:");
-  
-  if (num1 === "" || num2 === "") {
-    alert("Ups, looks like one of the fields is empty");
-  } else {
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-  
-    if (num1 < num2) {
-      const confirmOperation = confirm("Are you sure you want to continue the operation, the second number is bigger than the first one?");
-      if (confirmOperation) {
-        alert(`Difference: ${subtractNumbers(num1, num2)}`);
+  if (b.length === 0) {
+      return undefined;
+  }
+
+  b.push(-1500);
+  b.push(-1750);
+
+  let minValue = Infinity;
+  let minValue2 = Infinity;
+  let maxValue = -Infinity;
+
+  for(i = 0; i < b.length; i++) {
+      if (b[i] < minValue) {
+          minValue2 = minValue;
+          minValue = b[i];
+      } else if (b[i] < minValue2) {
+          minValue2 = b[i];
       }
-    } else {
-      alert(`Sum: ${addNumbers(num1, num2)}\nDifference: ${subtractNumbers(num1, num2)}\nMultiplication: ${multiplyNumbers(num1, num2)}\nDivision: ${divideNumbers(num1, num2)}`);
-    }
   }
-  
-  let arr = [3, 5, 1, 8, 2];
-  alert(`Minimum number: ${findMinimumNumber(arr)}\nMaximum number: ${findMaximumNumber(arr)}`);
+
+  for(i = 0; i < b.length; i++) {
+      if (b[i] > maxValue) {
+          maxValue = b[i]
+      }
+  }
+
+  console.log(minValue);
+  console.log(minValue2);
+  console.log(maxValue);
+
+}
+
+findMaxNumber()
